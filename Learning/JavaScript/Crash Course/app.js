6d3c314d8410d578b26ab1d43f5c9217d3b44f56
David <thedavidgilmore00@gmail.com>
2022-10-31 22:34:13 -0400

    Final CSS Project -- finished features and quality work sections

--- Learning/JavaScript/Crash Course/app.js
+++ Learning/JavaScript/Crash Course/app.js
@@ -1,2 +1,13 @@
-let str = 'David91'
-console.log(str[5 - 1])
+let cash = 50
+let price = 60
+let difference = cash-price
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} dollars change`)
+}
+else if (cash === price) {
+    console.log ('you paid the exact amoutn - have a nice day')
+}
+if (cash < price) {
+    console.log (`not enough money - you still owe ${difference * -1} dollars`)
+}
