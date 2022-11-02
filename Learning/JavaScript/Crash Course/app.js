b9ec92642b818bf33342d0293f475c8158b3cbd7
David <thedavidgilmore00@gmail.com>
2022-11-01 21:18:35 -0400

    Final CSS Project -- covered loops and conditionals in js lesson

--- Learning/JavaScript/Crash Course/app.js
+++ Learning/JavaScript/Crash Course/app.js
@@ -1,13 +1,3 @@
-// let count = 1;
-
-
-// while (count <= 100) {
-//     console.log(count)
-//     count = count +1;
-// }
-
-const str = 'Frontend Simplified'
-
-for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
+function toggleDarkMode () {
+    document.querySelector('body').classList.toggle("dark-theme")
 }
