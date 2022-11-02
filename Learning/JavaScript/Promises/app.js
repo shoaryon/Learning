1ab9c7a87c125c2bca9bd43c6922694a8d2cfef8
David <thedavidgilmore00@gmail.com>
2022-11-01 23:35:55 -0400

    Final CSS Project -- finsihed javascript promises

---
+++ Learning/JavaScript/Promises/app.js
@@ -0,0 +1,37 @@
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
+
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, 2000);
+  });
+}
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+  console.log(await getVideo(status));
+  }
+  catch (e) {
+    console.log(e)
+    videoRef.innerHTML = e;
+  }
+}
+
+main();
